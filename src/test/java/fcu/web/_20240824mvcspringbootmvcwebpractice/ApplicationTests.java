1195acbf41d08afcf5ef512f6aeed9488d9f27ba
alfo0924 <146694784+alfo0924@users.noreply.github.com>
2024-08-24 22:13:59 +0800

    v1.001

---
+++ src/test/java/fcu/web/_20240824mvcspringbootmvcwebpractice/ApplicationTests.java
@@ -0,0 +1,13 @@
+package fcu.web._20240824mvcspringbootmvcwebpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
